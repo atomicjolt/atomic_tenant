616c6e5bfdf6dc67bc2ccb9f83e8826baefee46b
Nick Benoit <nick.benoit@atomicjolt.com>
2022-08-16 16:45:45 -0600

    Add is to platform guid strategy

--- lib/atomic_tenant/deployment_manager/platform_guid_strategy.rb
+++ lib/atomic_tenant/deployment_manager/platform_guid_strategy.rb
@@ -7,9 +7,11 @@ module AtomicTenant
             "PlatformGuidStrategy"
           end
 
+          # return DeploymentStrategyResult
           def call(id_token:)
             decoded_token = JWT.decode(id_token, nil, false)
 
+            iss = decoded_token.dig(0, "iss")
             platform_guid = decoded_token.dig(0, AtomicLti::Definitions::TOOL_PLATFORM_CLAIM, "guid")
             target_link_uri = decoded_token.dig(0, AtomicLti::Definitions::TARGET_LINK_URI_CLAIM)
 
@@ -26,7 +28,7 @@ module AtomicTenant
 
             return DeploymentStrategyResult.new() if !app.present?
 
-            if(pinned = AtomicTenant::PinnedPlatformGuid.find_by(platform_guid: platform_guid, application_id: app.id))
+            if(pinned = AtomicTenant::PinnedPlatformGuid.find_by(iss: iss, platform_guid: platform_guid, application_id: app.id))
               DeploymentStrategyResult.new(application_instance_id: pinned.application_instance_id)
             else
               DeploymentStrategyResult.new()

c257a3a12ad9c4baded1f161703618754b0d7c6d
Nick Benoit <nick.benoit@atomicjolt.com>
2022-08-19 10:49:04 -0600

    Wrap middleware in try

--- lib/atomic_tenant.rb
+++ lib/atomic_tenant.rb
@@ -1,6 +1,7 @@
 require 'atomic_tenant/version'
 require 'atomic_tenant/deployment_manager/deployment_manager'
 require 'atomic_tenant/deployment_manager/platform_guid_strategy'
+require 'atomic_tenant/deployment_manager/client_id_strategy'
 require 'atomic_tenant/deployment_manager/deployment_manager_strategy'
 require 'atomic_tenant/engine'
 require 'atomic_tenant/current_application_instance_middleware'
--- lib/atomic_tenant/current_application_instance_middleware.rb
+++ lib/atomic_tenant/current_application_instance_middleware.rb
@@ -8,46 +8,49 @@ module AtomicTenant
     end
 
     def call(env)
-      request = Rack::Request.new(env)
+      begin
+        request = Rack::Request.new(env)
 
-      custom_strategies = AtomicTenant.custom_strategies || []
-      default_strategies = [
-        AtomicTenant::DeploymentManager::PlatformGuidStrategy.new,
-        AtomicTenant::DeploymentManager::ClientIdStrategy.new
-      ]
+        if env['atomic.validated.oauth_consumer_key'].present?
+          oauth_consumer_key = env['atomic.validated.oauth_consumer_key']
+          if ai = ApplicationInstance.find_by(lti_key: oauth_consumer_key)
+            env['atomic.validated.application_instance_id'] = ai.id
+          end
+        elsif env['atomic.validated.id_token'].present?
 
-      deployment_manager = AtomicTenant::DeploymentManager::DeploymentManager.new(custom_strategies.concat(default_strategies))
+          custom_strategies = AtomicTenant.custom_strategies || []
+          default_strategies = [
+            AtomicTenant::DeploymentManager::PlatformGuidStrategy.new,
+            AtomicTenant::DeploymentManager::ClientIdStrategy.new
+          ]
 
+          deployment_manager = AtomicTenant::DeploymentManager::DeploymentManager.new(custom_strategies.concat(default_strategies))
+          iss = env['atomic.validated.decoded_id_token']['iss']
+          deployment_id = env['atomic.validated.decoded_id_token'][AtomicLti::Definitions::DEPLOYMENT_ID]
+          id_token = env['atomic.validated.id_token']
 
-      iss = env['atomic.validated.lti_advantage.iss']
-      deployment_id = env['atomic.validated.lti_advantage.deployment_id']
-      id_token = env['atomic.validated.id_token']
+          if deployment = AtomicTenant::LtiDeployment.find_by(iss: iss, deployment_id: deployment_id)
+            env['atomic.validated.application_instance_id'] = deployment.application_instance_id
+          else
+            deployment = deployment_manager.link_deployment_id(id_token: id_token)
+             env['atomic.validated.application_instance_id'] = deployment.application_instance_id
+          end
 
-      if env['atomic.validated.oauth_consumer_key'].present?
-        oauth_consumer_key = env['atomic.validated.oauth_consumer_key']
-        if ai = ApplicationInstance.find_by(lti_key: oauth_consumer_key)
-          env['atomic.validated.application_instance_id'] = ai.id
-        end
-      elsif env['atomic.validated.id_token'].present?
+        elsif encoded_token(request).present?
+          token = encoded_token(request)
 
-        if deployment = AtomicTenant::LtiDeployment.find_by(iss: iss, deployment_id: deployment_id)
-          env['atomic.validated.application_instance_id'] = deployment.application_instance_id
-        else
-          deployment = deployment_manager.link_deployment_id(id_token: id_token)
-           env['atomic.validated.application_instance_id'] = deployment.application_instance_id
-        end
-
-      elsif encoded_token(request).present?
-        token = encoded_token(request)
-
-        # TODO: decoded token should be put on request
-        # TODO AuthToken should live in a dep
-        decoded_token = AuthToken.decode(token, nil, false)
-        if decoded_token.present? && decoded_token.first.present?
-          if app_instance_id = decoded_token.first['application_instance_id']
-            env['atomic.validated.application_instance_id'] = app_instance_id
+          # TODO: decoded token should be put on request
+          # TODO AuthToken should live in a dep
+          decoded_token = AuthToken.decode(token, nil, false)
+          if decoded_token.present? && decoded_token.first.present?
+            if app_instance_id = decoded_token.first['application_instance_id']
+              env['atomic.validated.application_instance_id'] = app_instance_id
+            end
           end
         end
+
+      rescue StandardError => e
+        Rails.logger.error("Error in current app instance middleware: #{e}")
       end
 
       @app.call(env)
